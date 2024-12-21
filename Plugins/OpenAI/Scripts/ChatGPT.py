fdd7d9772c78d6db76417ad1c1b4660d6d553917
Igor Garcia <igonildo7@gmail.com>
2024-12-21 17:18:52 -0300

    Logic to get data from key to my widget

--- Plugins/OpenAI/Scripts/ChatGPT.py
+++ Plugins/OpenAI/Scripts/ChatGPT.py
@@ -1,24 +1,21 @@
 import openai
 
-client = openai.OpenAI(
-    api_key=""
-)
-
 
 def get_messages(question: str):
     return [{"role": "user", "content": question.strip()}]
 
 
-def ask_question(question: str, api_key: str):
+def ask_question(api_key: str, question: str):
+    client = openai.OpenAI(
+        api_key=api_key
+    )
+
     openai.api_key = api_key
-    messages = get_messages(question)
+    MyMessage = get_messages(question)
     result = client.chat.completions.create(
         model="gpt-4o-mini",
         store=True,
-        messages=messages
+        messages=MyMessage
     )
 
-    return result.choices[0].message
-
-
-print(ask_question("qual a cor do ceu?", client.api_key))
+    return result.choices[0].message.content
