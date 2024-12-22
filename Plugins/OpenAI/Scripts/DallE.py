3082751508c8d7eb2f985f22070d9bf5201d3f83
Igor Garcia <igonildo7@gmail.com>
2024-12-21 22:37:56 -0300

    Finished logic to get dallE image and download it

--- Plugins/OpenAI/Scripts/DallE.py
+++ Plugins/OpenAI/Scripts/DallE.py
@@ -3,9 +3,21 @@ import requests
 import shutil
 
 
-def make_image(size: str, prompt: str):
-    response = openai.Image.create(prompt=prompt, n=1, size=size)
-    return response["data"][0]["url"]
+def make_image(size: str, prompt: str, api_key: str):
+    openai.api_key = api_key
+    # client = openai.OpenAI()
+
+    response = openai.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size=size,
+        quality="standard",
+        n=1,)
+
+    return response.data[0].url
+
+    # response = openai.Image.create(prompt=prompt, n=1, size=size)
+    # return response["data"][0]["url"]
 
 
 def download_image(url: str, filepath: str, filename: str):
@@ -19,6 +31,6 @@ def download_image(url: str, filepath: str, filename: str):
 
 
 def get_image(api_key: str, size: str, prompt: str, filename: str, filepath: str):
-    openai.api_key = api_key
-    image_url = make_image(size, prompt)
+
+    image_url = make_image(size, prompt, api_key)
     download_image(image_url, filepath, filename)
